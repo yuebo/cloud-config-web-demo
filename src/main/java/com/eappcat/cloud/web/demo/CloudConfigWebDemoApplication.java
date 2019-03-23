ebe9e3e4936ad0c6f4f60bbe878bfcca4ec3883c
Xuan Yue Bo <317728991@qq.com>
2019-03-24 00:36:04 +0800

    init

---
+++ src/main/java/com/eappcat/cloud/web/demo/CloudConfigWebDemoApplication.java
@@ -0,0 +1,13 @@
+package com.eappcat.cloud.web.demo;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class CloudConfigWebDemoApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(CloudConfigWebDemoApplication.class, args);
+    }
+
+}
